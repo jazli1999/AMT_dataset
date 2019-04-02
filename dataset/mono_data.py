5b1f35df3b421f9267aa05d9bd8f821839652e50
JasmineChiehLi <jasmine_chieh_li@outlook.com>
2019-04-02 15:57:13 +0800

    C4 applied

---
+++ dataset/mono_data.py
@@ -0,0 +1,24 @@
+import librosa, librosa.display, librosa.util
+import matplotlib.pyplot as plt
+import numpy as np
+import os, sys
+
+audio_path = '../dataset/audio/' + sys.argv[1]
+img_path = '../dataset/img/' + sys.argv[1]
+#argv[1] MIDI label
+
+wav_filename = sys.argv[2]
+y, sr = librosa.load(audio_path+ '/' +wav_filename)
+img_filename = '/' + (wav_filename.split('.'))[0] + '.png'
+
+print('CQTing ' + wav_filename + '....')
+C = np.abs(librosa.cqt(y, sr=sr, hop_length=64, n_bins = 84, bins_per_octave = 12))
+librosa.display.specshow(librosa.amplitude_to_db(C), sr=sr, x_axis='time', y_axis='cqt_note')
+print('Drawing ' + wav_filename + '....')
+plt.set_cmap('hot')
+plt.gca().xaxis.set_major_locator(plt.NullLocator())
+plt.gca().yaxis.set_major_locator(plt.NullLocator())
+plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+plt.margins(0, 0)
+plt.savefig(img_path+img_filename, format='png', transparent=False, dpi=300, pad_inches=0)
+print(img_path+img_filename + ' saved')
